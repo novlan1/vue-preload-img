f892f98ee869f55c95c57f88e87bab02ca9dd00e
guowangyang <guowangyang@tencent.com>
2020-12-14 15:30:16 +0800

    update: 增加自定义key

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 const VuePreLoadImg = {};
-const PRELOAD_IMAGES_KEY = 'VUE_PRELOAD_IMG_CONFIG';
+let PRELOAD_IMAGES_KEY = 'VUE_PRELOAD_IMG_CONFIG';
 
-/**
- * Vue预加载插件
- * 功能：在mounted钩子中，加载需要预加载的图片。
- *
- * 使用方法：
- *
- * 1. 引入
- * import VuePreLoadImg from 'vue-preload-img'
- *
- * 2. 使用
- * Vue.use(VuePreLoadImg)
- *
- * 3. 在data中加入要预加载的图片地址，如：
- *
- * data() {
- *   return {
- *     // ...
- *     VUE_PRELOAD_IMG_CONFIG: {
- *       delay: 1000, // 延迟毫秒数
- *       data: [
- *           '图片1.png'，
- *           '图片2.png'，
- *           '图片3.png'
- *        ]
- *     }
- *   }
- * }
- *
- */
-VuePreLoadImg.install = function (Vue) {
+VuePreLoadImg.install = function (Vue, options) {
+  if (options.key && typeof options.key === 'string') {
+    PRELOAD_IMAGES_KEY = options.key;
+  }
+
   Vue.mixin({
     mounted() {
       if (this.$data
